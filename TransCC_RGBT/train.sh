84850c5f7d5c9db1322b8546cb74e24ca33136a4
Zhan Meng <21302010039@m.fudan.edu.cn>
2024-05-18 20:32:34 +0800

    best

--- TransCC_RGBT/train.sh
+++ TransCC_RGBT/train.sh
@@ -1,6 +1,6 @@
 python train.py \
     --data-dir ../../rgbtcc_fdu/ \
     --save-dir ./model/ \
-    --pretrained_model pretrain.pth \
+    --pretrained_model ../../best_model/model_best.pth \
     --lr 1e-5 \
     --device 0
