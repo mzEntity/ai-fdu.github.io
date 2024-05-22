724743dbbca7148fab251995d62d7873777fb53c
Zhan Meng <21302010039@m.fudan.edu.cn>
2024-05-22 14:50:22 +0800

    train.sh

--- TransCC_RGBT/train.sh
+++ TransCC_RGBT/train.sh
@@ -1,5 +1,5 @@
 python train.py \
-    --data-dir ../../rgbtcc_fdu/ \
+    --data-dir ./rgbtcc_fdu/ \
     --save-dir ./model/ \
     --pretrained_model ../../best_model/model_best.pth \
     --batch-size 8 \
